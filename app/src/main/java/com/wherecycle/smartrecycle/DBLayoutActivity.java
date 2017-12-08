91834f971e7526cdd2f66f19e88f3a87e208aae2
J-Basquill <34130237+J-Basquill@users.noreply.github.com>
2017-12-08 10:06:45 +0000

    variables created

--- app/src/main/java/com/wherecycle/smartrecycle/DBLayoutActivity.java
+++ app/src/main/java/com/wherecycle/smartrecycle/DBLayoutActivity.java
@@ -2,8 +2,16 @@ package com.wherecycle.smartrecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 public class DBLayoutActivity extends AppCompatActivity {
+    private DatabaseReference myDB;
+    private ArrayList<> myDBArray = new ArrayList<>();
+    private ListView myDBList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
