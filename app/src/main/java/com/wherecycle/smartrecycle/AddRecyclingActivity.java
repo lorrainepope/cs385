c0134c9b53f1afdfeeb2f554cb11b62893a1e176
J-Basquill <34130237+J-Basquill@users.noreply.github.com>
2017-12-11 18:37:16 +0000

    Updated Input forn

--- app/src/main/java/com/wherecycle/smartrecycle/AddRecyclingActivity.java
+++ app/src/main/java/com/wherecycle/smartrecycle/AddRecyclingActivity.java
@@ -2,10 +2,12 @@ package com.wherecycle.smartrecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
+import com.wherecycle.smartrecycle.model.Locations;
 
 import javax.microedition.khronos.egl.EGLDisplay;
 
@@ -22,6 +24,24 @@ public class AddRecyclingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recycling);
 
+        eName = (EditText)findViewById(R.id.enterRName);
+        eContact = (EditText)findViewById(R.id.enterRContact);
+        eLat = (EditText)findViewById(R.id.enterRLat);
+        eLong = (EditText)findViewById(R.id.enterRLong);
 
+        submit = (Button)findViewById(R.id.submitRecycling);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String DBName = eName.getText().toString();
+                String DBContact = eContact.getText().toString();
+                
+                long DBLat = eLat.getText().;
+                long DBLong = eLong.toString();
+
+                Locations push = new Locations(DBName, DBLat, DBLong, DBContact);
+
+            }
+        });
     }
 }
