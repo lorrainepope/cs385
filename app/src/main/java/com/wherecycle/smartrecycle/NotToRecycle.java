9d067ae4d198365005fffa5334ff189d483a571b
lorrainemcgonigle <33573211+lorrainemcgonigle@users.noreply.github.com>
2017-12-30 16:23:13 +0000

    Benefits of recycling (#15)
    * Simply displays the benefits of recycling in a new activity
    
    * Scrollable list added with items not to recycle

---
+++ app/src/main/java/com/wherecycle/smartrecycle/NotToRecycle.java
@@ -0,0 +1,14 @@
+package com.wherecycle.smartrecycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotToRecycle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_to_recycle);
+        this.setTitle("What Not To Recycle");
+    }
+}
