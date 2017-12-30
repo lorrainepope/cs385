88b7c613e1291ba7db90fc91122e9882617a2de8
lorrainemcgonigle <33573211+lorrainemcgonigle@users.noreply.github.com>
2017-12-30 19:45:48 +0000

    James (#17)
    * Fixed variable typing issue
    
    * Database updates
    
    * Nothing to see here
    
    * Commit changes
    
    * Creating custom adapter
    
    * Finished updating array adapter, bugs still
    
    * FINALLLLY!!!!
    
    Click View all Bottle Banks to see database. May need to back out and reopen

---
+++ app/src/main/java/com/wherecycle/smartrecycle/LocationsAdapter.java
@@ -0,0 +1,51 @@
+package com.wherecycle.smartrecycle;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.ListViewAutoScrollHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.wherecycle.smartrecycle.model.Locations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by james on 27/12/2017.
+ */
+
+public class LocationsAdapter extends ArrayAdapter<Locations>{
+
+    private Context myContext;
+    private List<Locations> locationsList = new ArrayList<Locations>();
+
+    public LocationsAdapter(@NonNull Context context, int resource, @NonNull ArrayList<Locations> objects) {
+        super(context, resource, objects);
+        myContext = context;
+        locationsList = objects;
+    }
+
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        View listItem = convertView;
+        if(listItem == null){
+            listItem = LayoutInflater.from(myContext).inflate(R.layout.list_locations, parent, false);
+        }
+        Locations currLoc = locationsList.get(position);
+        TextView tvName = (TextView)listItem.findViewById(R.id.locName);
+        tvName.setText("Name: "+currLoc.getName());
+        TextView tvAdd = (TextView)listItem.findViewById(R.id.locAddress);
+        tvAdd.setText("Contact: "+currLoc.getAddress());
+        TextView tvLat = (TextView)listItem.findViewById(R.id.locLat);
+        tvLat.setText("Latitude: "+currLoc.getLat());
+        TextView tvLng = (TextView)listItem.findViewById(R.id.locLng);
+        tvLng.setText("Longitude: "+currLoc.getLng());
+
+        return listItem;
+    }
+}
