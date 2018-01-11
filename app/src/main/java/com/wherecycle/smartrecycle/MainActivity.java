dc9679edec7c4bc0ae2ed2de58f3a9bf348ad342
lorrainemcgonigle <33573211+lorrainemcgonigle@users.noreply.github.com>
2018-01-11 17:01:32 +0000

    James (#24)
    * Fixed variable typing issue
    
    * Database updates
    
    * Nothing to see here
    
    * Commit changes
    
    * Creating custom adapter
    
    * Finished updating array adapter, bugs still
    
    * FINALLLLY!!!!
    
    Click View all Bottle Banks to see database. May need to back out and reopen
    
    * Locations updated
    
    * Updated DB uploader
    
    * Commit DB layout updates and updated AddActivity
    
    * Added logic for filtering database
    
    * updated map functioanlity
    
    * updated map logic

--- app/src/main/java/com/wherecycle/smartrecycle/MainActivity.java
+++ app/src/main/java/com/wherecycle/smartrecycle/MainActivity.java
@@ -56,6 +56,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         sp = getSharedPreferences(fileName, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sp.edit();
+
+
         //editor.putString("nameKey", "A");
         //editor.apply();
 
