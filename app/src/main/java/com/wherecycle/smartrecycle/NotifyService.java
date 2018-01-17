afb7d0c39273de33e3d603f14f8e087a612b36c1
lorrainemcgonigle <33573211+lorrainemcgonigle@users.noreply.github.com>
2018-01-17 23:08:12 +0000

    Bin day test (#26)
    * WorkingNotificationService
    
    * Notification fix, Cleanup
    
    Notification will now push a notification the day before the bin collection date

---
+++ app/src/main/java/com/wherecycle/smartrecycle/NotifyService.java
@@ -0,0 +1,85 @@
+// referenced from http://blog.blundellapps.co.uk/notification-for-a-user-chosen-time/
+
+package com.wherecycle.smartrecycle;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.IBinder;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.os.Binder;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+
+
+public class NotifyService extends Service {
+    /**
+     * Class for clients to access
+     */
+    public class ServiceBinder extends Binder {
+        NotifyService getService() {
+            return NotifyService.this;
+        }
+    }
+
+    // Unique id to identify the notification.
+    private static final int NOTIFICATION = 123;
+    // Name of an intent extra we can use to identify if this service was started to create a notification
+    public static final String INTENT_NOTIFY = "com.blundell.tut.service.INTENT_NOTIFY";
+    // The system notification manager
+    private NotificationManager mNM;
+
+    @Override
+    public void onCreate() {
+        Log.i("NotifyService", "onCreate()");
+        mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i("LocalService", "Received start id " + startId + ": " + intent);
+
+        // If this service was started by out AlarmTask intent then we want to show our notification
+        if(intent.getBooleanExtra(INTENT_NOTIFY, false))
+            showNotification();
+
+        // We don't care if this service is stopped as we have already delivered our notification
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    // This is the object that receives interactions from clients
+    private final IBinder mBinder = new ServiceBinder();
+
+    /**
+     * Creates a notification and shows it in the OS drag-down status bar
+     */
+    private void showNotification() {
+        // This is the 'title' of the notification
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this);
+
+        //Create the intent that’ll fire when the user taps the notification//
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.androidauthority.com/"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        mBuilder.setContentIntent(pendingIntent);
+
+        mBuilder.setSmallIcon(R.mipmap.ic_mobius);
+        mBuilder.setContentTitle("Bin Collection Day Reminder");
+        mBuilder.setContentText("Bin Collection Day Reminder");
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        mNotificationManager.notify(001, mBuilder.build());
+    }
+}
